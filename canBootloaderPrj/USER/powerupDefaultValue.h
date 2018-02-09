f22a9ac45908c2f62caec3625280aa6d546e9742
XiaoxingChen <cxx@seer-robotics.com>
2018-02-09 14:11:36 +0800

    Modify: add lost file

---
+++ canBootloaderPrj/USER/powerupDefaultValue.h
@@ -0,0 +1,17 @@
+#ifndef POWERUP_DEFAULT_VALUE_H
+#define POWERUP_DEFAULT_VALUE_H
+
+enum powerupVarEnum
+{
+	VAR_BOOT_OPTI = 0,
+	VAR_NUM_OF_VAR,
+	VarNum
+};
+
+enum
+{
+	POWERUP_VAR_ADDR = 0x08007800
+};
+
+#endif
+//end of file
