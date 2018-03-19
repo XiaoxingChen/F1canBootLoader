08c97365cbc7e3249f6178ae007e05ccbafab0c5
XiaoxingChen <cxx@seer-robotics.com>
2018-03-19 15:42:36 +0800

    modify: add lost file

---
+++ uartBootloaderPrj/USER/powerupDefaultValue.cpp
@@ -0,0 +1,10 @@
+#include "powerupOption.h"
+#include "stmflash.h"
+
+extern const uint32_t DEFAULT_POWERUP_VAL[pvf::VarNum] = 
+{
+	BOOT_PARAM_APP, 
+	pvf::VarNum
+};
+
+//end of file
---
+++ uartBootloaderPrj/USER/powerupDefaultValue.h
@@ -0,0 +1,18 @@
+#ifndef POWERUP_DEFAULT_VALUE_H
+#define POWERUP_DEFAULT_VALUE_H
+
+enum powerupVarEnum
+{
+	VAR_BOOT_OPTI = 0,
+	VAR_NUM_OF_VAR,
+	VAR_SECU_KEY,
+	VarNum
+};
+
+enum
+{
+	POWERUP_VAR_ADDR = 0x0800FC00
+};
+
+#endif
+//end of file
