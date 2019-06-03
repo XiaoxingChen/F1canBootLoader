7ff3bba6da660f6ab145e2d6b8c3c87d765215c1
darboy <974782852@qq.com>
2019-06-03 16:54:34 +0800

    modified the stmflash

--- F1HAL/stmflash.c
+++ F1HAL/stmflash.c
@@ -158,7 +158,64 @@ int STMFLASH_write_bytes(uint32_t appxaddr,uint8_t *buf,uint16_t len)
 }
 
 
+#include "stm32f10x_flash.h"
 
+#define StartServerManageFlashAddress    ((u32)0x08036000)//读写起始地址（内部flash的主存储块地址从0x08036000开始）
+
+//从指定地址开始写入多个数据
+void FLASH_WriteMoreData(uint32_t startAddress,uint16_t *writeData,uint16_t countToWrite)
+{    
+    uint32_t offsetAddress=startAddress - FLASH_BASE;               //计算去掉0X08000000后的实际偏移地址
+  uint32_t sectorPosition=offsetAddress/STM_SECTOR_SIZE;            //计算扇区地址，对于STM32F103VET6为0~255
+  uint32_t sectorStartAddress=sectorPosition*STM_SECTOR_SIZE+FLASH_BASE;    //对应扇区的首地址
+    uint16_t dataIndex;
+    
+  if(startAddress<FLASH_BASE||((startAddress+countToWrite*2)>=(FLASH_BASE + STM_SECTOR_SIZE * STM32_FLASH_SIZE)))
+  {
+    return;//非法地址
+  }
+  FLASH_Unlock();         //解锁写保护
+
+  FLASH_ErasePage(sectorStartAddress);//擦除这个扇区
+  
+  for(dataIndex=0;dataIndex<countToWrite;dataIndex++)
+  {
+    FLASH_ProgramHalfWord(startAddress+dataIndex*2,writeData[dataIndex]);
+  }
+  
+  FLASH_Lock();//上锁写保护
+}
+
+//读取指定地址的半字(16位数据)
+uint16_t FLASH_ReadHalfWord(uint32_t address)
+{
+  return *(__IO uint16_t*)address; 
+}
+
+//从指定地址开始读取多个数据
+void FLASH_ReadMoreData(uint32_t startAddress,uint16_t *readData,uint16_t countToRead)
+{
+  uint16_t dataIndex;
+  for(dataIndex=0;dataIndex<countToRead;dataIndex++)
+  {
+    readData[dataIndex]=FLASH_ReadHalfWord(startAddress+dataIndex*2);
+  }
+}
+
+void write_to_flash(void)
+{
+    u16 buff[1200];
+    u16 count_len = 2272 / 2;
+    FLASH_WriteMoreData(StartServerManageFlashAddress,buff,count_len);
+}
+
+void read_from_flash(void)
+{
+    u16 buff[1200];
+    u16 count_len = 2272 / 2;
+    FLASH_WriteMoreData(StartServerManageFlashAddress,buff,count_len);
+
+ }
 
 
 
